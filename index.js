e2ac71c6870399e627c0bdd0734abdb643797e24
Yogesh Kumar <yogeshkumar.pc00@gmail.com>
2024-03-15 14:50:06 +0530

    added api

--- index.js
+++ index.js
@@ -3,6 +3,40 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT
 
+const obj = {
+    "login": "hiteshchoudhary",
+    "id": 11613311,
+    "node_id": "MDQ6VXNlcjExNjEzMzEx",
+    "avatar_url": "https://avatars.githubusercontent.com/u/11613311?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/hiteshchoudhary",
+    "html_url": "https://github.com/hiteshchoudhary",
+    "followers_url": "https://api.github.com/users/hiteshchoudhary/followers",
+    "following_url": "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+    "gists_url": "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/hiteshchoudhary/subscriptions",
+    "organizations_url": "https://api.github.com/users/hiteshchoudhary/orgs",
+    "repos_url": "https://api.github.com/users/hiteshchoudhary/repos",
+    "events_url": "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/hiteshchoudhary/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Hitesh Choudhary",
+    "company": null,
+    "blog": "https://www.youtube.com/c/HiteshChoudharydotcom",
+    "location": "India",
+    "email": null,
+    "hireable": null,
+    "bio": "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+    "twitter_username": "hiteshdotcom",
+    "public_repos": 82,
+    "public_gists": 4,
+    "followers": 17616,
+    "following": 0,
+    "created_at": "2015-03-23T13:03:25Z",
+    "updated_at": "2024-02-12T12:11:50Z"
+}
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -15,7 +49,7 @@ app.get('/login',(req,res)=>{
     res.send("<h1> Login Page</h1>")
 })
 app.get('/json',(req,res)=>{
-    res.json({name:"yogesh"})
+    res.json(obj)
 })
 
 app.listen(port, () => {
